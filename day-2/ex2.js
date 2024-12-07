5ed7f08517bb4aa68a661dd5afd2e0498871c0a0
rezina0611 <han06110@naver.com>
2024-12-07 18:54:34 +0900

    init

---
+++ day-2/ex2.js
@@ -0,0 +1,39 @@
+/**
+ * 
+ * 병원 원무시스템
+ * 
+ * - 환자
+ *  - 이름 string
+ *  - 성별 MALE | FEMALE
+ *  - 병명 string
+ *  - 퇴원까지 남은 일자ㅏ : NUMBER
+ * 
+ * 
+ * john male 팔부상 2
+ * doe male 복통 3
+ * anne female 다리골절 4
+ * jain female 허리디스크 14일
+ * 
+ * 전체 환자 목록 조회
+ * 오늘 퇴원해야 하는 환자 조회
+ * 며칠 뒤 ( 5일 뒤)에 퇴원해야하는 환자 목록
+ */
+
+const TODAY = 1
+
+const patientsList = []
+
+function getPatientList(){
+
+}
+
+/**
+ * 오늘 퇴원이 필요한 환자 목록
+ */
+function getTodayNeedLeftPatientList(){}
+
+/**
+ * 오늘 기준 며칠 뒤에 퇴원이 필요한환자 목록
+ * @param {*} day 
+ */
+function getNeedLeftByDayPatientList(day){}
