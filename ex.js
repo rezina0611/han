56ac35f6313147c9657df82fcc7b5a1407e0c71c
Raymond <akdfhr2@gmail.com>
2024-12-07 18:50:14 +0900

    Add initial implementation of utility functions in ex.js

---
+++ ex.js
@@ -0,0 +1,28 @@
+// 1. "Hello, World!"를 출력하는 함수 작성
+function sayHello() {}
+
+// 2. 두 숫자를 더하는 함수 작성
+function add(a, b) {}
+
+// 3. 이름을 받아 'Hello name!' 을 출력하는 함수
+function greetName(name) {}
+
+// 4. a,b 두 수중 큰 숫자 반환 함수
+function getMax(a, b) {}
+
+// 5. 짝수/홀수 판단 함수 짝수면 '짝수', 홀수면 '홀수'를 반환
+function checkEvenOdd(num) {}
+
+// 6. a,b 두 수를 곱한 값을 반환하는 화살표 함수 작성
+const multiply = () => {};
+
+// 7. 배열의 합 구하기
+function sumArray(arr) {}
+
+// 8. 숫자 배열 중 원하는 숫자를 찾는 함수
+function findNumber(arr, target) {}
+
+// 예제
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(findNumber(numbers, 5)); // true
+console.log(findNumber(numbers, 11)); // false
